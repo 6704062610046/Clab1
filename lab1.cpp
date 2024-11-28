47295ac49287e9dd20925f7d2dbaa56bace95e23
6704062610046 <s6704062610046@email.kmutnb.ac.th>
2024-11-28 14:43:33 +0700

    Commit 3 chill chill

--- lab1.cpp
+++ lab1.cpp
@@ -3,9 +3,8 @@
 int main() {
 	int n1;
 	int n2;
-	printf("n1=<Enter num1> \nn2=<Enter num2>\n");
-	scanf("%d",&n1);
-	scanf("%d", &n2);
+	printf("n1=<Enter num1>\nn2=<Enter num2>\n");
+	scanf("%d %d",&n1 , &n2);
 	if(n1>n2){
 		printf("n1 is more n2");
 	}
